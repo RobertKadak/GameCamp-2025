603ba464cca2373e0ee38fc43babceba5a2b9561
Daniel Zapryanov <dzapryanov05@gmail.com>
2025-03-29 10:15:08 +0200

    feat: Add moving enemy tween.
    Loop animations going left and right. To fix: enemy doesn't flip, needs to wait at standpoints.

--- entities/enemies/moving/enemy_moving.gd
+++ entities/enemies/moving/enemy_moving.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 @export var vision_range: float = 200
 @export var reaction_time: float = 1.0
-@export var movement_speed: float = 350
+@export var movement_speed: float = 150
+@export var patrol_distance: float = 100
 
 @onready var raycast = $RayCast2D
 @onready var reaction_timer = $ReactionTime
+@onready var patrol_wait_timer = $PatrolWait
 @onready var area2D = $Area2D
+@onready var enemy = self
 
 
 var player = null
@@ -14,6 +17,7 @@ var player = null
 func _ready():
 	area2D.body_entered.connect(_on_Area2D_body_entered)
 	reaction_timer.timeout.connect(_on_ReactionTime_timeout)
+	patrol()
 
 #CHECK IF SMALL GUY IS WITHIN GETTING-FUCKED RANGE OF ENEMY
 func _on_Area2D_body_entered (body):
@@ -33,3 +37,17 @@ func check_line_of_sight():
 func _on_ReactionTime_timeout():
 	if player:
 		player.die()
+
+func _on_PatrolWait_timeout():
+	pass 
+
+func patrol():	
+	var animation_tween = create_tween()
+	var duration = 2
+	var start_pos = enemy.position
+	var left_point = start_pos - Vector2(patrol_distance, 0)
+	var right_point = start_pos + Vector2(patrol_distance,0)
+	
+	animation_tween.tween_property(enemy, "position", left_point, duration).set_trans(Tween.TRANS_LINEAR)
+	animation_tween.tween_property(enemy, "position", right_point, duration).set_trans(Tween.TRANS_LINEAR)
+	animation_tween.set_loops()
